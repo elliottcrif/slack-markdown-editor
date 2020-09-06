174f40eca5badc26459cab3ad43a999679d72d05
Elliott Crifasi <ecrif23@gmail.com>
2020-09-06 13:46:12 -0500

    WIP

---
+++ src/util/lexer.js
@@ -0,0 +1,124 @@
+// Create reference instance
+import Lexer from "lex";
+import React from "react";
+
+export class Parser {
+  constructor() {
+    this.lexer = new Lexer();
+    this.v = [];
+
+    /**
+     * Italics
+     */
+    this.lexer.addRule(/\_[^_\n]+\_/g, (text) => {
+      this.v.push({
+        type: "ITALICS",
+        text: text.replace(/\_/g, ""),
+      });
+    });
+
+    /**
+     * Bold
+     */
+    this.lexer.addRule(/\*[A-z0-9]+\*/g, (text) => {
+      this.v.push({
+        type: "BOLD",
+        text: text.replace(/\*/g, ""),
+      });
+    });
+
+    /**
+     * Strikethrough
+     */
+    this.lexer.addRule(/~[A-z0-9]+~/g, (text) => {
+      this.v.push({
+        type: "STRIKETHROUGH",
+        text,
+      });
+    });
+
+    /**
+     * List Item
+     */
+    this.lexer.addRule(/-[ A-z0-9]+/g, (text) => {
+      this.v.push({
+        type: "LIST_ITEM",
+        text,
+      });
+    });
+
+    /**
+     * BLOCK_QUOTE
+     */
+    this.lexer.addRule(/>[ A-z0-9]+\n*/g, (text) => {
+      this.v.push({
+        type: "BLOCK_QUOTE",
+        text: text.replace("> ", ""),
+      });
+    });
+
+    /**
+     * Paragraph
+     */
+    this.lexer.addRule(/([a-z0-9A-Z]*\?*\.*\,*\s)*/g, (text) => {
+      this.v.push({ type: "PARAGRAPH", text });
+    });
+
+    /**
+     * NEW_LINE
+     */
+    this.lexer.addRule(/\n/g, (text) => {
+      this.v.push({ type: "NEW_LINE", text });
+    });
+  }
+
+  toHTML(token) {
+    switch (token.type) {
+      case "ITALICS":
+        return (
+          <span key={token.text} style={{ fontStyle: "italic" }}>
+            {token.text}
+          </span>
+        );
+      case "BOLD":
+        return (
+          <span key={token.text} style={{ fontWeight: "bold" }}>
+            {token.text}
+          </span>
+        );
+      case "STRIKETHROUGH":
+        return (
+          <span key={token.text} style={{ textDecoration: "line-through" }}>
+            {token.text}
+          </span>
+        );
+      case "BLOCK_QUOTE":
+        return (
+          <blockquote
+            key={token.text}
+            style={{ padding: "2em", borderLeft: "3px solid" }}
+          >
+            {token.text}
+          </blockquote>
+        );
+      case "NEW_LINE":
+        return <br></br>;
+      default:
+        return token.text;
+    }
+  }
+
+  parse(input) {
+    try {
+      this.v = [];
+      this.lexer.setInput(input);
+      this.lexer.lex();
+
+      return this.v.map((token) => this.toHTML(token));
+    } catch (e) {
+      return input;
+    } finally {
+      console.log(this.v);
+    }
+  }
+}
